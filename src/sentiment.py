b53424729988bde87fade22afd336ec27beefc4f
Sumaia Parveen <56892114+SumaiaParveen@users.noreply.github.com>
2021-09-13 23:15:14 -0700

    Update sentiment.py

--- src/sentiment.py
+++ src/sentiment.py
@@ -29,7 +29,7 @@ def download_link(object_to_download, download_filename, download_link_text):
 def sent():
 
     #st.subheader("Unsupervised Sentiment Analysis")
-    st.markdown("<h3 style='text-align: center;'>Rule Based Sentiment Analysis</h3>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center;'>Unsupervised Sentiment Analysis</h3>", unsafe_allow_html=True)
     uploaded_file = st.file_uploader("Choose a file")
 
     while True:
